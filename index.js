453d4893a4e7aa38c2953d25b2929c5869059e8e
Emerson TODE <Emerson TODE>
2017-03-27 23:41:37 -0300

    fixed

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 process.env.Path += `;${__dirname}\\node_modules\\chromedriver\\lib\\chromedriver\\`;
-console.log(process.env);
 
 const webDriver = require('selenium-webdriver');
 const by = webDriver.By;
@@ -14,3 +13,10 @@ driver.wait(until.elementLocated(by.xpath('//*[@id="email"]')));
 driver.findElement(by.xpath('//*[@id="email"]')).sendKeys('');
 driver.findElement(by.xpath('//*[@id="pass"]')).sendKeys('');
 driver.findElement(by.xpath('//*[@id="loginbutton"]')).click();
+
+driver.get('https://www.facebook.com/events/birthdays');
+driver.wait(driver.findElement(by.xpath('//*[@id="facebook"]/body/div[15]/div[1]')));
+driver.findElement(by.xpath('//*[@id="facebook"]/body/div[15]/div[1]')).click();
+driver.wait(driver.findElement(by.xpath('//*[@id="events_birthday_view"]/div[1]/ul/li/div/div/div/div[2]/div/div[2]/div[1]/div[1]/a')));
+nome = driver.findElement(by.xpath('//*[@id="events_birthday_view"]/div[1]/ul/li/div/div/div/div[2]/div/div[2]/div[1]/div[1]/a')).getText();
+driver.findElement(by.xpath('//*[@id="u_0_v"]')).sendKeys('Parabens ' + nome.value + ' tudo de bom xD');
