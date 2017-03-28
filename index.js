ad198eb68e14266bde892d489ff28cafcc877fd4
emerson <emerson.oliveira@tegra.me>
2017-03-28 10:40:04 -0300

    Navega ate pagina e manda msg

--- index.js
+++ index.js
@@ -15,8 +15,12 @@ driver.findElement(by.xpath('//*[@id="pass"]')).sendKeys('');
 driver.findElement(by.xpath('//*[@id="loginbutton"]')).click();
 
 driver.get('https://www.facebook.com/events/birthdays');
-driver.wait(driver.findElement(by.xpath('//*[@id="facebook"]/body/div[15]/div[1]')));
+driver.wait(until.elementLocated(by.xpath('//*[@id="facebook"]/body/div[15]/div[1]')));
 driver.findElement(by.xpath('//*[@id="facebook"]/body/div[15]/div[1]')).click();
-driver.wait(driver.findElement(by.xpath('//*[@id="events_birthday_view"]/div[1]/ul/li/div/div/div/div[2]/div/div[2]/div[1]/div[1]/a')));
-nome = driver.findElement(by.xpath('//*[@id="events_birthday_view"]/div[1]/ul/li/div/div/div/div[2]/div/div[2]/div[1]/div[1]/a')).getText();
-driver.findElement(by.xpath('//*[@id="u_0_v"]')).sendKeys('Parabens ' + nome.value + ' tudo de bom xD');
+driver.wait(until.elementLocated(by.xpath('//*[@id="events_birthday_view"]/div[2]/div[2]/ul/li/div/div/div/div[2]/div/div[2]/div[1]')));
+var tagNome = driver.findElement(by.xpath('//*[@id="events_birthday_view"]/div[2]/div[2]/ul/li/div/div/div/div[2]/div/div[2]/div[1]'));
+tagNome.getText().then(nome => {
+  nome = nome.split(" ", 1);
+  driver.findElement(by.xpath('//*[@id="u_0_v"]')).sendKeys('Parabens ' + nome + ' tudo de bom xD');
+  driver.findElement(by.xpath('//*[@id="u_0_v"]')).sendKeys(key.ENTER);
+});
